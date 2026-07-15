6b45eb02db66c2e5b12bae2d8981c64b1c4e20eb
mysailingcoach <mysailingcoach1@gmail.com>
2026-07-15 13:29:23 +0100

    Remove old postcss.config.js - use postcss.config.cjs instead

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,3 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+// This file has been moved to postcss.config.cjs
+// Do not use this file - it exists only to prevent conflicts
+throw new Error('Use postcss.config.cjs instead');
